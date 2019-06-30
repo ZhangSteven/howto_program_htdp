a09e370dbe271fc881bd367bab0bc4f4ff827361
Zhang Shutao <steven.shutao@gmail.com>
2019-06-30 22:21:35 +0800

    some more exercises added

---
+++ ch02_ex11.rkt
@@ -0,0 +1,11 @@
+;; The first three lines of this file were inserted by DrRacket. They record metadata
+;; about the language level of this file in a form that our tools can easily process.
+#reader(lib "htdp-beginner-reader.ss" "lang")((modname ch02_ex11) (read-case-sensitive #t) (teachpacks ((lib "image.rkt" "teachpack" "2htdp") (lib "universe.rkt" "teachpack" "2htdp"))) (htdp-settings #(#t constructor repeating-decimal #f #t none #f ((lib "image.rkt" "teachpack" "2htdp") (lib "universe.rkt" "teachpack" "2htdp")) #f)))
+; distance between origin to point (x, y)
+;
+(define (distance x y)
+  (sqrt (+ (squa x) (squa y))))
+
+
+(define (squa x)
+  (* x x))
---
+++ ch02_ex26.rkt
@@ -0,0 +1,12 @@
+;; The first three lines of this file were inserted by DrRacket. They record metadata
+;; about the language level of this file in a form that our tools can easily process.
+#reader(lib "htdp-beginner-reader.ss" "lang")((modname ch02_ex26) (read-case-sensitive #t) (teachpacks ((lib "image.rkt" "teachpack" "2htdp") (lib "universe.rkt" "teachpack" "2htdp"))) (htdp-settings #(#t constructor repeating-decimal #f #t none #f ((lib "image.rkt" "teachpack" "2htdp") (lib "universe.rkt" "teachpack" "2htdp")) #f)))
+; Run this program with the stepper, so we understand exactly how the
+; output is evaluated.
+;
+(define (string-insert s i)
+  (string-append (substring s 0 i)
+                 "_"
+                 (substring s i)))
+
+(string-insert "helloworld" 6)
